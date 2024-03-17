dbf089b482cb3f0d53ef4a924e09f364049a3b44
sree1724 <163710611+sree1724@users.noreply.github.com>
2024-03-17 11:36:23 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,48 @@
+#include<stdio.h> 
+void main() 
+{ 
+char STR[100],PAT[100],REP[100],ans[100]; 
+int i,j,m,k,flag=0; 
+printf("\nEnter the MAIN string: \n"); 
+gets(STR); 
+printf("\nEnter a PATTERN string: \n"); 
+gets(PAT); 
+printf("\nEnter a REPLACE string: \n"); 
+gets(REP); 
+i = m = j = 0; 
+while ( STR[m] != '\0') 
+{ 
+// Checking for Match
+if ( STR[m] == PAT[i] ) 
+{ 
+i++; m++; 
+flag=1; 
+if ( PAT[i] == '\0') 
+{ 
+//copy replace string in ans string 
+for(k=0; REP[k] != '\0';k++,j++) 
+ans[j] = REP[k]; 
+//i=0; 
+//c=m; 
+
+} 
+}
+else //mismatch
+{ 
+ans[j] = STR[m];
+j++; 
+m++; 
+//m = c; 
+//i=0; 
+} 
+} 
+if(flag==0) 
+{ 
+printf("Pattern doesn't found!!!"); 
+} 
+else 
+{ 
+ans[j] = '\0'; 
+printf("\nThe RESULTANT string is:%s\n" ,ans); 
+} 
+}
